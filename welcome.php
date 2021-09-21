d22830383aa0cd0b2cb0b42aa8959987302925a8
MURAMV ISHNU VARDHAN REDDY <87742060+VishnuVardhanReddyMuram@users.noreply.github.com>
2021-09-21 17:39:15 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,35 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+     <h2>Click on upload to save the files in database</h2>
+     <form action="upload.php" method="post" enctype="multipart/form-data">
+     Select Image File to Upload:
+    <input type="file" name="file">
+    <input type="submit" name="submit" value="Upload">
+   
+
+</form>
+
+
+</body>
+</html>
